5a02dfb90dc1022ee2ade26c432166d2c7cfa39c
xnm-sdksdk <n.mendonca@protonmail.com>
2024-11-19 19:33:38 +0000

    Initial commit

---
+++ src/main/java/com/xnm/chatapp/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.xnm.chatapp.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/xnm/chatapp/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.xnm.chatapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+
+    }
+}
